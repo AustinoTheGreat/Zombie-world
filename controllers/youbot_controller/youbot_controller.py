f5ec039a63b263bc50a13696d4f1b49c37206893
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2021-12-03 15:21:07 -0500

    object detection

--- controllers/youbot_controller/youbot_controller.py
+++ controllers/youbot_controller/youbot_controller.py
@@ -8,7 +8,15 @@ from youbot_zombie import *
 #------------------CHANGE CODE BELOW HERE ONLY--------------------------
 #define functions here for making decisions and using sensor inputs
     
-    
+import cv2
+import numpy as np
+
+class image_obj:
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y
+
+SPEED_STEP = 1.48
 
 def base_set_wheel_speed_helper(speeds = [], wheels = [], *args):
     for i in range(0, 4):
@@ -17,20 +25,52 @@ def base_set_wheel_speed_helper(speeds = [], wheels = [], *args):
         wheels[i].setVelocity(speeds[i])
         
 def move_forward(speed, wheels = []):
-    speeds = [speed*4, speed*4, speed*4, speed*4]
+    speeds = [speed*SPEED_STEP, speed*SPEED_STEP, speed*SPEED_STEP, speed*SPEED_STEP]
     base_set_wheel_speed_helper(speeds, wheels)
     
 def move_backward(speed, wheels = []):
-    speeds = [-speed*4, -speed*4, -speed*4, -speed*4]
+    speeds = [-speed*SPEED_STEP, -speed*SPEED_STEP, -speed*SPEED_STEP, -speed*SPEED_STEP]
     base_set_wheel_speed_helper(speeds, wheels)
     
 def turn_left(speed, wheels = []):
-    speeds = [speed*4, 2, speed*4, 2]
+    speeds = [speed*SPEED_STEP, SPEED_STEP, speed*SPEED_STEP, SPEED_STEP]
     base_set_wheel_speed_helper(speeds, wheels)
 
 def turn_right(speed, wheels = []):
-    speeds = [2, speed*4, 2, speed*4]
+    speeds = [SPEED_STEP, speed*SPEED_STEP, SPEED_STEP, speed*SPEED_STEP]
     base_set_wheel_speed_helper(speeds, wheels)
+
+def get_image_from_camera(camera):
+    """
+    Take an image from the camera device and prepare it for OpenCV processing:
+    - convert data type,
+    - convert to RGB format (from BGRA), and
+    - rotate & flip to match the actual image.
+    """
+    img = camera.getImageArray()
+    img = np.asarray(img, dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    return cv2.flip(img, 1)
+
+def get_img_obj(img, lower_range, higher_range):
+
+    objs = []
+    mask = cv2.inRange(img, np.array(lower_range), np.array(higher_range))
+    
+    # Find segmented contours and their center
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # largest_contour = max(contours, key=cv2.contourArea)
+    # largest_contour_center = cv2.moments(largest_contour)
+    
+    for item in contours:
+        center = cv2.moments(item)
+        if center['m00'] != 0:
+            center_x = int(center['m10'] / center['m00'])
+            center_y = int(center['m01'] / center['m00'])
+            objs.append(image_obj(center_x, center_x))
+    return objs   
+     
 
 
 
@@ -173,6 +213,41 @@ def main():
         
         #make decisions using inputs if you choose to do so
         
+        red_lower_range = [50, 150, 0]
+        red_higher_range = [200, 230, 255]
+        
+        orange_lower_range = [50, 150, 0]
+        orange_higher_range = [200, 230, 255]
+        
+        yellow_lower_range = [50, 150, 0]
+        yellow_higher_range = [200, 230, 255]
+        
+        pink_lower_range = [50, 150, 0]
+        pink_higher_range = [200, 230, 255]
+    
+        blue_lower_range = [50, 150, 0]
+        blue_higher_range = [200, 230, 255]
+ 
+        green_lower_range = [50, 150, 0]
+        green_higher_range = [200, 230, 255]
+        
+        purple_lower_range = [50, 150, 0]
+        purple_higher_range = [200, 230, 255]
+        
+        aqua_lower_range = [50, 150, 0]
+        aqua_higher_range = [200, 230, 255]
+        
+        
+        front_img = get_image_from_camera(camera1)
+        front_img = cv2.cvtColor(front_img, cv2.COLOR_RGB2HSV)
+        
+        berries = get_img_obj(front_img, red_lower_range, red_higher_range)
+        for item in berries:
+            print(item.x, item.y)
+        
+        
+        # Speed can be set from 0 to 10
+        move_forward(3, wheels)
         
         
          
