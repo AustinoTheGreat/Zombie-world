average fixed

controllers/youbot_controller/youbot_controller.py
@@ -302,7 +302,7 @@ def get_zombie_angle_weighted_average(zombie_locations):
         return 0 #average is 0
     average = int(np.degrees(np.arctan([y/x]))[0])
     if average < 0:
-        average = 360 - average
+        average = 360 + average
     if average > 360:
         average = average - 360
     print("Average " + str(average))